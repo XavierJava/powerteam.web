b4866498759be8cc23c0d9b98aff29b3b0e8ca5b
Xavier Li (Wicresoft North America Ltd) <v-chunli@microsoft.com>
2018-09-11 01:34:25 +0000

    update

--- src/js/public/SideBar.js
+++ src/js/public/SideBar.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react';
 class Sidebar extends Component {
     render() {
         return (
-            <ul>
-          xavier
+            <ul className="nav nav-sidebar" id="childNode" style={{display:'block'}}>
+            <li className="active"><a href="index.html">Overview <span className="sr-only">(current)</span></a></li>
+            <li><a href="">Project</a></li>
+            <li><a href="Delivery.html">Delivery</a></li>
+            <li><a href="">Documentations</a></li>
+            <li><a href="">Support</a></li>
+	        <li><a href="">Support</a></li>
           </ul>
         );
       }
